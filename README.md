d32f838db1f8dd15144106dd8ddea0840e986479
rlourie <super.sanek20142@yandex.ru>
2022-07-20 22:42:56 +0300

    Add allure

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# task_1_testing_saggests
+# Тестирование саджестов.
+
+Для запуска локально
+
+```
+pytest tests
+```
+
+## Для создания allure отчета
+
+```
+pytest --alluredir results
+allure serve results
+```
+
+## Для тестирования поисковой строки по другим категориям
+
+Нужно изменить `self.url = "https://go.mail.ru/` в файле `test_pytest.py`  на адресс тестируемого раздела, например
+на `https://go.mail.ru/search_social?fr=main&frm=main`
--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,5 +1,7 @@
 import pytest
-import time
+import allure
+import pathlib
+from allure_commons.types import AttachmentType
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -7,7 +9,8 @@ from selenium.webdriver.common.by import By
 class Test:
     def setup(self):
         self.url = "https://go.mail.ru/"
-        self.driver = webdriver.Chrome()
+        ex_path = pathlib.Path.home() / 'Desktop' / 'task_1_testing_sadgests' / 'tests' / 'chromedriver'
+        self.driver = webdriver.Chrome(executable_path=str(ex_path))
         self.driver.implicitly_wait(3)
 
     @staticmethod
@@ -35,7 +38,7 @@ class Test:
         count_suggest = len(self.driver.find_elements(By.CLASS_NAME, "DesktopSuggests-suggest"))
         self.driver.get(self.url)
         for i in range(1, count_suggest + 1):
-            x_path_suggest = f"//ul[@class='DesktopSuggests-suggestsListWrapper MainPageContainer-suggestsListWrapper DesktopSuggests-keyboardUnControl']/li[{i}]"
+            x_path_suggest = f"//ul[contains(@class, 'suggestsListWrapper')]/li[{i}]"
             search_form = self.driver.find_element(By.NAME, 'q')
             search_form.click()
             search_form.send_keys(text)
@@ -58,7 +61,7 @@ class Test:
         search_form.send_keys(text)
         count_suggest = len(self.driver.find_elements(By.CLASS_NAME, "DesktopSuggests-suggest"))
         for i in range(1, count_suggest + 1):
-            x_path_suggest = f"//ul[@class='DesktopSuggests-suggestsListWrapper MainPageContainer-suggestsListWrapper DesktopSuggests-keyboardUnControl']/li[{i}]"
+            x_path_suggest = f"//ul[contains(@class, 'suggestsListWrapper')]/li[{i}]"
             current_suggest = self.driver.find_element(By.XPATH, x_path_suggest)
             current_text = current_suggest.text
             if ru_1_en_2 == 0:
@@ -75,57 +78,95 @@ class Test:
         return counter, count_suggest
 
     # Тесты на запросы с саджестов.
+    @allure.feature('Тесты на запросы с саджестов.')
+    @allure.story('Проверяем переход всех саджестов с "тест"')
     def test_case_1(self):
         a, b = Test.check_all_request_suggests(self, 'тест')
         assert ((a == b) and (a != 0 and b != 0))
 
+    @allure.feature('Тесты на запросы с саджестов.')
+    @allure.story('Проверяем переход всех саджестов с "test"')
     def test_case_2(self):
         a, b = Test.check_all_request_suggests(self, 'test')
         assert ((a == b) and (a != 0 and b != 0))
 
+    @allure.feature('Тесты на запросы с саджестов.')
+    @allure.story('Проверяем переход всех саджестов с ""')
     def test_case_3(self):
         a, b = Test.check_all_request_suggests(self, '')
         assert ((a == b) and (a != 0 and b != 0))
 
+    @allure.story('Проверяем переход всех саджестов с "~"')
+    @allure.feature('Тесты на запросы с саджестов.')
     def test_case_4(self):
         a, b = Test.check_all_request_suggests(self, '~')
         assert ((a == b) and (a != 0 and b != 0))
 
+    @allure.story('Проверяем переход всех саджестов с "1234"')
+    @allure.feature('Тесты на запросы с саджестов.')
     def test_case_5(self):
         a, b = Test.check_all_request_suggests(self, '1234')
         assert ((a == b) and (a != 0 and b != 0))
 
-    # Тесты на вхождение запроса в саджест односложные.
+    @allure.story('Проверяем переход всех саджестов с "много тестов"')
+    @allure.feature('Тесты на запросы с саджестов.')
     def test_case_6(self):
+        a, b = Test.check_all_request_suggests(self, 'много тестов')
+        assert ((a == b) and (a != 0 and b != 0))
+
+    @allure.story('Проверяем переход всех саджестов с "big test"')
+    @allure.feature('Тесты на запросы с саджестов.')
+    def test_case_7(self):
+        a, b = Test.check_all_request_suggests(self, 'big test')
+        assert ((a == b) and (a != 0 and b != 0))
+
+    # Тесты на вхождение запроса в саджест односложные.
+    @allure.story('Проверяем вхождение запроса в саджесты Запрос: "тест"')
+    @allure.feature('Тесты на вхождение запроса в саджест односложные.')
+    def test_case_8(self):
         a, b = Test.check_all_in_suggests(self, 'тест')
         assert ((a == b) and (a != 0 and b != 0))
 
-    def test_case_7(self):
+    @allure.story('Проверяем вхождение запроса в саджесты Запрос: "test"')
+    @allure.feature('Тесты на вхождение запроса в саджест односложные.')
+    def test_case_9(self):
         a, b = Test.check_all_in_suggests(self, 'test')
         assert ((a == b) and (a != 0 and b != 0))
 
-    def test_case_8(self):
+    @allure.story('Проверяем вхождение запроса в саджесты Запрос: "*"')
+    @allure.feature('Тесты на вхождение запроса в саджест односложные.')
+    def test_case_10(self):
         a, b = Test.check_all_in_suggests(self, '*')
         assert ((a == b) and (a != 0 and b != 0))
 
-    def test_case_9(self):
+    @allure.story('Проверяем вхождение запроса в саджесты Запрос: "1234"')
+    @allure.feature('Тесты на вхождение запроса в саджест односложные.')
+    def test_case_11(self):
         a, b = Test.check_all_in_suggests(self, '1234')
         assert ((a == b) and (a != 0 and b != 0))
 
     # Тесты на вхождние транслитирации односложные.
-    def test_case_10(self):
+    @allure.story('Проверяем транслитирацию Запрос: "еуые"')
+    @allure.feature('Тесты на вхождние транслитирации односложные.')
+    def test_case_12(self):
         a, b = Test.check_all_in_suggests(self, 'еуые', 2)
         assert ((a == b) and (a != 0 and b != 0))
 
-    def test_case_11(self):
+    @allure.story('Проверяем транслитирацию Запрос: "ntcn"')
+    @allure.feature('Тесты на вхождние транслитирации односложные.')
+    def test_case_13(self):
         a, b = Test.check_all_in_suggests(self, 'ntcn', 1)
         assert ((a == b) and (a != 0 and b != 0))
 
     # Тест на некоректные данные для саджеста
-    def test_case_12(self):
+    @allure.story('Проверяем на пустые саджесты Запрос: "rfgthygjh"')
+    @allure.feature('Тест на некоректные данные для саджеста')
+    def test_case_14(self):
         a, b = Test.check_all_in_suggests(self, 'rfgthygjh')
         assert (a == 0 and b == 0)
 
-    def test_case_13(self):
+    @allure.story('Проверяем на пустые саджесты Запрос: "акперинго"')
+    @allure.feature('Тест на некоректные данные для саджеста')
+    def test_case_15(self):
         a, b = Test.check_all_in_suggests(self, 'акперинго')
         assert (a == 0 and b == 0)
